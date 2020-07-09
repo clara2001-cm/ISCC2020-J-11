e080b6fb75fa2de9ead77e91951f898377c3e346
clara2001-cm <67279165+clara2001-cm@users.noreply.github.com>
2020-07-09 16:20:30 +0200

    Add files via upload

---
+++ ajout-produit.php
@@ -0,0 +1,24 @@
+<?php
+
+function connect_to_database(){
+   
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $databasename = "BaseTest01";
+} 
+
+try{
+    $pdo = new PDO("mysql:host=$servername;dbname=$databasename",$username, $password);
+    $pdo->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully";
+    
+    return $pdo;
+}      catch (PDOException $e) {
+        
+    echo "Connection failed: ". $e->getMessage();
+    }
+ 
+    $sql="INSERT INTO produit VALUES('T-shirt noir','t_shirt coton de couleur noire',15.50,10)";
+?>
---
+++ maj-produit.php
@@ -0,0 +1,34 @@
+<?php
+
+    function connect_to_database(){
+       
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $databasename = "BaseTest01";
+    } 
+    
+    try{
+        
+        $pdo->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        echo "Connected successfully";
+        
+        return $pdo;
+    }      catch (PDOException $e) {
+            
+        echo "Connection failed: ". $e->getMessage();
+        }
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $databasename = "BaseTest01";
+
+$pdo = new PDO("mysql:host=$servername;dbname=$databasename",$username, $password);
+$SQL = "UPDATE produit SET quantité=5 WHERE ID= 4";  
+
+$stmt= $pdo->prepare($sql);
+$stmt->execute();
+
+?>
